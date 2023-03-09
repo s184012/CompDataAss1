f5079c5528e3f5942362bf7702da5dcf8d252d49
Simon Majgaard <simon.majgaard@gmail.com>
2023-03-09 15:15:21 +0100

    fix raw

--- create_raw.r
+++ create_raw.r
@@ -3,8 +3,8 @@ library(tidyverse)
 df <- read_csv('case1Data.txt', na = "NaN")
 glimpse(df)
 
-df |> 
+raw <- df |> 
   rename_with(function(x) sub(pattern = " ", replacement = "", x=x)) |> 
   names()
 
-write_csv(df, file = "raw.csv")
+write_csv(raw, file = "raw.csv")
